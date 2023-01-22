022cfc16d059168187367158da0b81e948220f7c
KittenCat1080ti <71938947+KittenCat1080ti@users.noreply.github.com>
2023-01-22 14:56:40 -0500

    Added game piece targeting

---
+++ src/main/java/frc/robot/subsystems/GamePieceTargeting.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class GamePieceTargeting extends SubsystemBase {
+  /** Creates a new GamePeiceTargeting. */
+  private Limelight m_gamepiececamera;
+  public GamePieceTargeting() {
+    m_gamepiececamera = new Limelight("limelight-gamepeice");
+
+    m_gamepiececamera.setPipeline(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  // -----------  Generic Target Functions -----
+
+  public boolean isTarget(){
+    if (m_gamepiececamera.isTargetPresent() == 0) { return false; }
+    else { return true; }
+  }
+
+  public double getTargetHorAngle() {
+    double Angle = m_gamepiececamera.getHorizontalTargetOffsetAngle();
+    return Angle;
+  }
+  
+  /** Returns vertical angle of target (deg)*/
+  public double getTargetVertAngle() {
+    return m_gamepiececamera.getVerticalTargetOffsetAngle();
+  }
+}
