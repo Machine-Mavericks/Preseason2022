f896435fe0a485686de8ab58e9bcb955f632cb83
KatieFardella <katie.fardella@gmail.com>
2023-02-03 20:49:26 -0500

    LED work

---
+++ src/main/java/frc/robot/commands/instant/LEDColourControl.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.instant;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Limelight;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class LEDColourControl extends InstantCommand {
+  public LEDColourControl() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    Limelight limelight = new Limelight("hello");
+
+    if (limelight.isTargetPresent()==1){
+      
+    }
+  
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+}
